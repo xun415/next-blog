e9fa501fb881acc619065f59ae0f8fd964cc404c
박태훈 <xun415@naver.com>
2024-01-09 11:16:02 +0900

    🍱: profile 이미지 변경

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,7 +10,7 @@ const gothic = Nanum_Gothic({
 
 const Profile = () => {
     return <article className={'flex flex-col items-center gap-1'}>
-        <Image className={'rounded-full'} src="/images/profile.png" alt="프로필 이미지" width={200} height={200}/>
+        <Image className={'rounded-full'} src="/images/profile.jpeg" alt="프로필 이미지" width={200} height={200}/>
         <h2 className={`text-lg font-bold text-center`}>Hi, I'm Taehun</h2>
         <p className={'text-sm'}>프론트엔드 개발자</p>
         <Link className={'rounded-lg p-1 font-medium text-white text-sm transition bg-blue-500 hover:bg-blue-400'} href={'/contact'}>Contact Me</Link>
